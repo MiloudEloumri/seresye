6ed90831c2ce06141deeacc4d3d381eae711cc7e
MiloudEloumri <miloud.eloumri@gmail.com>
2021-11-22 21:21:31 -0500

    Updated seresyet_12.erl

--- test/seresyet_12.erl
+++ test/seresyet_12.erl
@@ -15,43 +15,35 @@
 given([initialized, with, data], Engine, _) ->
     {ok,
      seresye_engine:assert(Engine,
-                           [{male, bob}, {mail, joe}, {male, corrado},
-                            {female, sara}, {parent, bob, joe}, {parent, sara, bob},
+                           [{male, bob},
+                            {mail, joe},
+                            {male, corrado},
+                            {female, sara},
+                            {parent, bob, joe},
+                            {parent, sara, bob},
                             {parent, corrado, bob}])};
-given([a, seresye, engine, that, is, initialized, with,
-       state],
-      _, _) ->
+given([a, seresye, engine, that, is, initialized, with, state], _, _) ->
     Engine0 = seresye_engine:new([]),
-    Engine1 = lists:foldl(fun (X, Engine1) ->
-                                  seresye_engine:add_rule(Engine1,
-                                                          {seresyet_simple_relatives, X})
-                          end,
-                          Engine0, [mother, father, grandfather, grandmother]),
+    Engine1 =
+        lists:foldl(fun(X, Engine1) ->
+                       seresye_engine:add_rule(Engine1, {seresyet_simple_relatives, X})
+                    end,
+                    Engine0,
+                    [mother, father, grandfather, grandmother]),
     {ok, Engine1}.
 
-'when'([seresye, propagation, is, complete],
-       Engine, _) ->
+'when'([seresye, propagation, is, complete], Engine, _) ->
     %% Propagation happens immediately, so nothing to do here. Its
     %% just a placeholder
     {ok, Engine}.
 
-then([the, per, engine, state, is, retrievable],
-     Engine, _) ->
+then([the, per, engine, state, is, retrievable], Engine, _) ->
     {ok, {Engine, seresye_engine:get_client_state(Engine)}};
-then([contains, the, data, populated, by, the, rules],
-     State = {_, InternalState}, _) ->
-    ?assertMatch(true,
-                 (lists:member({grandmother, sara, joe},
-                               InternalState))),
-    ?assertMatch(true,
-                 (lists:member({grandfather, corrado, joe},
-                               InternalState))),
-    ?assertMatch(true,
-                 (lists:member({mother, sara, bob}, InternalState))),
-    ?assertMatch(true,
-                 (lists:member({mother, sara, bob}, InternalState))),
-    ?assertMatch(true,
-                 (lists:member({father, corrado, bob}, InternalState))),
-    ?assertMatch(true,
-                 (lists:member({father, bob, joe}, InternalState))),
+then([contains, the, data, populated, by, the, rules], State = {_, InternalState}, _) ->
+    ?assertMatch(true, lists:member({grandmother, sara, joe}, InternalState)),
+    ?assertMatch(true, lists:member({grandfather, corrado, joe}, InternalState)),
+    ?assertMatch(true, lists:member({mother, sara, bob}, InternalState)),
+    ?assertMatch(true, lists:member({mother, sara, bob}, InternalState)),
+    ?assertMatch(true, lists:member({father, corrado, bob}, InternalState)),
+    ?assertMatch(true, lists:member({father, bob, joe}, InternalState)),
     {ok, State}.
